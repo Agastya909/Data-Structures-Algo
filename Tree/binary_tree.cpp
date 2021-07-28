4a7b5f22eb07cf37f44f47df9a97078eea099a7b
agastya9 <krishnarajawat909@gmail.com>
2021-07-28 22:50:36 +0530

    Tree

---
+++ Tree/binary_tree.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std;
+
+class treeNode 
+{
+    int data;
+    treeNode *left;
+    treeNode *right;
+    treeNode()
+    {
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class binaryTree 
+{
+
+};
+
+int main()
+{
+    return 0;
+}
