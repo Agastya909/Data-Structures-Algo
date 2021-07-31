06d89893ca6a6fbf78a0baa216d1d6883a587b7d
agastya9 <krishnarajawat909@gmail.com>
2021-07-31 13:19:01 +0530

    Search Added

--- Tree/binary_tree.cpp
+++ Tree/binary_tree.cpp
@@ -17,6 +17,7 @@ public:
     void printInorder(node *root);
     void preOrder(node *root);
     void postOrder(node *root);
+    int findElement(node *root, int find, int returnValue);
     node *addNode(int data);
     node *makeBST(node *root, int data);
     BST()
@@ -57,7 +58,10 @@ void BST::inputData()
     while (operation != 0)
     {
         cout << "1 to enter data in BST\n";
-        cout << "2 to print the tree\n";
+        cout << "2 to print Inorder traversal\n";
+        cout << "3 to print Preorder traversal\n";
+        cout << "4 to print Postorder traversal\n";
+        cout << "5 to search for an element in the tree\n";
         cout << "0 to quit\n";
         cin >> operation;
         switch (operation)
@@ -75,15 +79,29 @@ void BST::inputData()
             printInorder(root);
             cout << endl;
             break;
-        case 4:
+        case 3:
             cout << "The tree in Preorder traversal is : ";
             preOrder(root);
             cout << endl;
             break;
-        case 5:
+        case 4:
             cout << "The tree in Postorder traversal is : ";
             postOrder(root);
             cout << endl;
+            break;
+        case 5:
+            int find, returnValue = -1;
+            cout << "Enter element that has to be searched : ";
+            cin >> find;
+            returnValue = findElement(root, find, returnValue);
+            if (returnValue == 1)
+            {
+                cout << "The element is present in the tree.\n";
+            }
+            else
+            {
+                cout << "The element is not present in the tree.\n";
+            }
             break;
         }
     }
@@ -129,6 +147,37 @@ void BST::postOrder(node *root)
         postOrder(root->right);
         cout << root->key << "  ";
     }
+}
+
+int BST::findElement(node *root, int find, int returnValue)
+{
+    if (root->key == find)
+    {
+        returnValue = 1;
+    }
+    else if (root->key > find)
+    {
+        if (root->left != NULL)
+        {
+            returnValue = findElement(root->left, find, returnValue);
+        }
+        else
+        {
+            returnValue = 0;
+        }
+    }
+    else if (root->key < find)
+    {
+        if (root->right != NULL)
+        {
+            returnValue = findElement(root->right, find, returnValue);
+        }
+        else
+        {
+            returnValue = 0;
+        }
+    }
+    return returnValue;
 }
 
 int main()
